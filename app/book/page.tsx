294013129b8285efdb644185b03355db95ae0f20
alecan27 <alazar.damena01@gmail.com>
2024-03-13 09:41:07 +0300

    book page updated

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,56 +1,61 @@
 'use client';
 import Nav from '../components/Nav';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Book() {
-    return(
-        <>
-        <Nav />
-        <section className="bg-white h-screen text-black">
-          <div className="container mx-auto pt-5 overflow-x-auto">
-              <table className="bg-slate-300 table table-xs table-pin-rows table-pin-cols">
-              <thead>
-                  <tr className="bg-slate-400">
-                      <th className="border border-slate-600 px-4 py-2">Title</th>
-                      <th className="border border-slate-600 px-4 py-2">Author</th>
-                      <th className="border border-slate-600 px-4 py-2">Edition</th>
-                      <th className="border border-slate-600 px-4 py-2">Price</th>
-                      <th className="border border-slate-600 px-4 py-2">Status</th>
-                      <th className="border border-slate-600 px-4 py-2">Overview</th>
-                      <th className="border border-slate-600 px-4 py-2">Type</th>
-                      <th className="border border-slate-600 px-4 py-2">Genere</th>
-                      <th className="border border-slate-600 px-4 py-2">Action</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td className="border border-slate-600 px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                      <td className="border border-slate-600 px-4 py-2">Malcolm Lockyer</td>
-                      <td className="border border-slate-600 px-4 py-2">1961</td>
-                      <td className="border border-slate-600 px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                      <td className="border border-slate-600 px-4 py-2">Malcolm Lockyer</td>
-                      <td className="border border-slate-600 px-4 py-2">1961</td>
-                      <td className="border border-slate-600 px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                      <td className="border border-slate-600 px-4 py-2">Malcolm Lockyer</td>
-                      <td className="border border-slate-600 px-4 py-2">1961</td>
-                  </tr>
-                  <tr>
-                      <td className="border border-slate-600 px-4 py-2">Witchy Woman</td>
-                      <td className="border border-slate-600 px-4 py-2">The Eagles</td>
-                      <td className="border border-slate-600 px-4 py-2">1972</td>
-                      <td className="border border-slate-600 px-4 py-2">Witchy Woman</td>
-                      <td className="border border-slate-600 px-4 py-2">The Eagles</td>
-                      <td className="border border-slate-600 px-4 py-2">1972</td>
-                      <td className="border border-slate-600 px-4 py-2">Witchy Woman</td>
-                      <td className="border border-slate-600 px-4 py-2">The Eagles</td>
-                      <td className="border border-slate-600  px-4 py-2">1972</td>
-                  </tr>
-              </tbody>
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    const response = await fetch('/api/addbook');
+    const datas = await response.json();
+    setData(datas);
+  };
+  console.log(data);
+
+  return (
+    <>
+      <Nav />
+      <section className="bg-white h-screen text-black">
+        <div className="container mx-auto pt-5 overflow-x-auto">
+          <table className="bg-slate-300 table table-xs table-pin-rows table-pin-cols">
+            <thead>
+              <tr className="bg-slate-400">
+                <th className="border border-slate-600 px-4 py-2">Title</th>
+                <th className="border border-slate-600 px-4 py-2">Author</th>
+                <th className="border border-slate-600 px-4 py-2">Edition</th>
+                <th className="border border-slate-600 px-4 py-2">Price</th>
+                <th className="border border-slate-600 px-4 py-2">Status</th>
+                <th className="border border-slate-600 px-4 py-2">Overview</th>
+                <th className="border border-slate-600 px-4 py-2">Type</th>
+                <th className="border border-slate-600 px-4 py-2">Genere</th>
+                <th className="border border-slate-600 px-4 py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((list) => (
+                <tr key={list._id}>
+                  <td className="border border-slate-600 px-4 py-2">{list.Title}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Author}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Edition}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Price}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Status}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Overview}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Type}</td>
+                  <td className="border border-slate-600 px-4 py-2">{list.Genere}</td>
+                  <td className="border border-slate-600 px-4 py-2"><a>Edit</a> <a>Delete</a></td>
+                </tr>
+              ))}
+            </tbody>
           </table>
-      </div>
+        </div>
       </section>
-</>
-    );
-};
+    </>
+  );
+}
 
 export default Book;
--- pages/api/addbook/index.js
+++ pages/api/addbook/index.js
@@ -6,7 +6,9 @@ const Book = require('../../../models/BookModels')
   try{
     await mongoose.connect(`mongodb+srv://alazar-da:ale123@cluster0.eki1tpt.mongodb.net/bookstore?retryWrites=true&w=majority`)
         console.log("connected")
-        const book = new Book(req.body);
+
+        if(req.method==="POST"){
+          const book = new Book(req.body);
         await book.save().then(
           ()=>{
             res.status(200).json(
@@ -16,13 +18,18 @@ const Book = require('../../../models/BookModels')
             )
           }
         )
+        }
+        else if(req.method==="GET"){
+          const data  = await Book.find()
+          res.status(200).json(data)
+        }
+
   }
   catch{
     res.status(500).json({
       message:"something went wrong"
     })
   }
-
 
 }
 
